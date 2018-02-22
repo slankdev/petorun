740aa4385c65e52541aca23af826bef1176a4ff9
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-02-22 12:44:25 +0000

    2018.02.22-12:44

--- rtnl/main.cc
+++ rtnl/main.cc
@@ -4,15 +4,20 @@
 #include <string.h>
 #include "netlink.h"
 
-
 int main(int argc, char** argv)
 {
+#if 1
+  slankdev::netlink netlink;
+  netlink.dump_route();
+  // netlink.add_route();
+  // netlink.del_route();
+#else
   slankdev::socketfd sock;
 	sock.socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-
   int nlsock = sock.get_fd();
   netlink_request_route_dump(nlsock);
   netlink_read_until_done(nlsock);
+#endif
 }
 
 
